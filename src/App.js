8096a894689e4706f31f3b738c725a218771cf87
Fil <concatofilippo94@gmail.com>
2024-07-12 23:35:16 +0200

    .env file to protect API key

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import Search from "./components/Search";
 import Weather from "./components/Weather";
-import Map from "./components/Map"
+import Map from "./components/Map";
 import axios from "axios";
 
 const App = () => {
     const [weatherData, setWeatherData] = useState(null);
-    const [loading, setLoading] = useState(null);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
     const fetchWeather = async (city) => {
-        const openWeatherApiKey = "ea9db42196c3888afde15c54def98c4c";
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + openWeatherApiKey + "&units=metric";
+        const openWeatherApiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherApiKey}&units=metric`;
 
         try {
             setLoading(true);
@@ -19,7 +19,7 @@ const App = () => {
             const response = await axios.get(url);
             setWeatherData(response.data);
         } catch (e) {
-            setError("Could not fetch weather data. ErrorMessageId: ");
+            setError("Could not fetch weather data.");
             console.error(e);
         } finally {
             setLoading(false);
