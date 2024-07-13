81c27274c7f449cfcb10822184cbc00953f51055
Fil <concatofilippo94@gmail.com>
2024-07-13 02:32:10 +0200

    removed Recursive function

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,28 +3,10 @@ import React from 'react';
 const Weather = ({ data }) => {
     const { description, icon } = data.weather[0];
     const { temp, temp_min, temp_max, feels_like } = data.main;
-    const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     const degreeSymbol = '\u00B0C';
 
-    const renderData = (data) => {
-        return Object.entries(data).map(([key, value]) => {
-            if (typeof value === 'object' && value !== null) {
-                return (
-                    <div key={key}>
-                        <strong>{key}:</strong>
-                        <div style={{ paddingLeft: '20px' }}>
-                            {renderData(value)}
-                        </div>
-                    </div>
-                );
-            }
-            return (
-                <div key={key}>
-                    <strong>{key}:</strong> {value.toString()}
-                </div>
-            );
-        });
-    };
+
 
     return (
         <div className="weather-info">
@@ -35,7 +17,6 @@ const Weather = ({ data }) => {
             <p>Temperature Minimum: {Math.round(temp_min)} {degreeSymbol}</p>
             <p>Temperature Maximum: {Math.round(temp_max)} {degreeSymbol}</p>
             <p>Feels Like: {Math.round(feels_like)} {degreeSymbol}</p>
-            {renderData(data)}
         </div>
     );
 };
