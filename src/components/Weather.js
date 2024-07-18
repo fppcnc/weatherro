83e4d7f244d322cfa7331725bb781c85ed21e341
Fil <concatofilippo94@gmail.com>
2024-07-18 20:28:52 +0200

    removed redundant math.round

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -15,10 +15,10 @@ const Weather = ({data}) => {
                 <h2>Weather Information for {data.name}, {data.sys['country']}</h2>
                 <div className="weather-info-content">
                     <div className="weather-info-text">
-                        <p>Temperature: {Math.round(temp)} {degreeSymbol}</p>
-                        <p>Temperature Minimum: {Math.round(temp_min)} {degreeSymbol}</p>
-                        <p>Temperature Maximum: {Math.round(temp_max)} {degreeSymbol}</p>
-                        <p>Feels Like: {Math.round(feels_like)} {degreeSymbol}</p>
+                        <p>Temperature: {temp} {degreeSymbol}</p>
+                        <p>Temperature Minimum: {temp_min} {degreeSymbol}</p>
+                        <p>Temperature Maximum: {temp_max} {degreeSymbol}</p>
+                        <p>Feels Like: {feels_like} {degreeSymbol}</p>
                     </div>
                     <div className="img-container">
                         <img src={iconUrl} alt={description} />
