c89f4879cbdb0558570e17cfbc7dc92a482b491e
Fil <concatofilippo94@gmail.com>
2024-07-18 21:00:16 +0200

    Tooltip takes into account of responsiveness

--- src/hooks/useTooltip.js
+++ src/hooks/useTooltip.js
@@ -1,26 +1,30 @@
 import { useState } from 'react';
 
 const useTooltip = () => {
-    const [tooltipStyle, setTooltipStyle] = useState({ visibility: 'hidden', left: '0px', top: '0px' });
+    const [tooltipStyle, setTooltipStyle] = useState({ visibility: 'hidden', opacity: 0 });
 
     const handleMouseEnter = () => {
-        setTooltipStyle(prevStyle => ({ ...prevStyle, visibility: 'visible' }));
+        setTooltipStyle((prevStyle) => ({ ...prevStyle, visibility: 'visible', opacity: 1 }));
     };
 
     const handleMouseMove = (e) => {
-        setTooltipStyle({ visibility: 'visible', left: `${e.clientX + 10}px`, top: `${e.clientY + 10}px` });
+        const windowWidth = window.innerWidth;
+        const tooltipWidth = 300; // Assuming the tooltip width is 300px
+        let left = e.clientX + 10;
+        let top = e.clientY + 10;
+
+        if (windowWidth - e.clientX < tooltipWidth) {
+            left = e.clientX - tooltipWidth - 10;
+        }
+
+        setTooltipStyle((prevStyle) => ({ ...prevStyle, left: `${left}px`, top: `${top}px` }));
     };
 
     const handleMouseLeave = () => {
-        setTooltipStyle(prevStyle => ({ ...prevStyle, visibility: 'hidden' }));
+        setTooltipStyle((prevStyle) => ({ ...prevStyle, visibility: 'hidden', opacity: 0 }));
     };
 
-    return {
-        tooltipStyle,
-        handleMouseEnter,
-        handleMouseMove,
-        handleMouseLeave,
-    };
+    return { tooltipStyle, handleMouseEnter, handleMouseMove, handleMouseLeave };
 };
 
 export default useTooltip;
